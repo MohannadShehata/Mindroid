1da052bfb685f620d4de66c41dc25828d91afa76
Saad <msi.6ix@gmail.com>
2021-11-06 19:19:09 -0400

    not even working

--- Mindroid.py
+++ Mindroid.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm, Chat
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5765763432dsf2423423d'
 
 posts = [
     {
@@ -8,6 +12,7 @@ posts = [
     'content' : 'First post content',
     'date_posted' : 'April 20, 2018'
     },
+
     {
         'author' : 'Hanah',
         'title' : 'Blog Post 2',
@@ -24,6 +29,21 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/chat")
+def chat():
+    form = ChatMessage()
+    return render_template('chat.html', title='Chat', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
