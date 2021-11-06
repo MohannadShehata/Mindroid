dbb7e5391c245ae4af1cb4c80f6bdaf297e9d3d7
Yannick Longval <yannicklongval2014@gmail.com>
2021-11-06 16:00:58 -0400

    Created desktop design
    without the text messaging.

---
+++ mindroid-react-app/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './Navbar.css';
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <img src="/img/MINDROID1.png" alt="MINDROID1"/>
+                </div>
+            </nav>      
+        </>
+    )
+}
+
+export default Navbar
