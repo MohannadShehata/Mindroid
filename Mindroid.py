169b37784a547b7b29b03430809fc56efe31ec6c
Saad <msi.6ix@gmail.com>
2021-11-07 01:22:26 -0500

    mm

--- Mindroid.py
+++ Mindroid.py
@@ -1,34 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 #from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5765763432dsf2423423d'
 
-posts = [
-    {
-    'author' : 'Corey',
-    'title' : 'Blog Post 1',
-    'content' : 'First post content',
-    'date_posted' : 'April 20, 2018'
-    },
-
-    {
-        'author' : 'Hanah',
-        'title' : 'Blog Post 2',
-        'content' : 'Set post content',
-        'date_posted' : 'March 20, 2018'
-    }
-]
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home", methods=["POST"])
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
+
+@app.route()
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
